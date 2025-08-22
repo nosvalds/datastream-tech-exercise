cd43ffdcf86c5c40bc127ef3097b05408284e5e4
Nik Osvalds <nosvalds@gmail.com>
2025-08-22 09:02:28 -0400

    adding styling across the page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import datastreamIcon from '$lib/assets/datastream-main.svg';
+	import socialIcons from '$lib/assets/icons.social.svg';
 
 	let { children } = $props();
 </script>
@@ -9,4 +11,16 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<header class="flex items-center bg-[#152d42]">
+	<a href="/"><img src={datastreamIcon} alt="Datastream Icon" class="m-4 h-10 invert" /></a>
+	<h1 class="mt-3 text-2xl font-bold text-white">Data Analyzer</h1>
+</header>
+
 {@render children?.()}
+
+<footer class="flex justify-between bg-[#152d42]">
+	<p class="ml-4 p-2 text-white">© 2025</p>
+	<a class="mr-4 p-2" href="https://github.com/nosvalds/datastream-tech-exercise" target="_blank">
+		<img alt="GitHub" height="24" src={socialIcons + '#github'} width="24" class="invert" />
+	</a>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,39 +2,65 @@
 	import type { PageProps } from './$types';
 
 	let { data, form }: PageProps = $props();
-    console.log(form);
+	console.log(form);
 </script>
 
-<h1>DataStream Data Analyzer</h1>
-<h2>Upload and Analyze Your Data</h2>
+<div class="flex justify-center gap-4 p-4">
+	<div class=" flex max-w-3xl flex-col gap-4">
+		<h2 class="text-xl">Upload and Analyze Your Data</h2>
+		<p class=" ">
+			Use the form below to upload a dataset file in CSV format. Then you can select a monitoring
+			location and calculate the average water temperature for the selected location.
+		</p>
 
-<form method="post" enctype="multipart/form-data" action="?/upload">
-  <div>
-    <label for="dataset">Choose a dataset file to Analyze:</label>
-    <input type="file" id="dataset" name="dataset" accept=".csv" />
-  </div>
-  <div>
-    <button>Upload</button>
-  </div>
-</form>
+		<form
+			class="flex items-end gap-4"
+			method="post"
+			enctype="multipart/form-data"
+			action="?/upload"
+		>
+			<div>
+				<label class="mb-2 block" for="dataset">File</label>
+				<input
+					class="text-sm text-stone-500 file:mr-5 file:rounded file:border-[1px] file:bg-stone-50 file:px-3 file:py-1 file:text-xs file:font-medium file:text-stone-700 hover:file:cursor-pointer hover:file:bg-blue-50 hover:file:text-blue-700"
+					type="file"
+					id="dataset"
+					name="dataset"
+					accept=".csv"
+				/>
+			</div>
+			<div>
+				<button
+					class="rounded border border-[#d0375b] bg-[#d0375b] px-4 py-2 text-white hover:bg-white hover:text-[#d0375b]"
+					>Upload</button
+				>
+			</div>
+		</form>
 
-{#if form?.success}
-	<p>Successfully uploaded file: {form.fileName}</p>
-    <form method="post" action="?/calculate">
-        <div>
-            <label for="location">Select Monitoring Location:</label>
-            <select id="location" name="location">
-                {#each Array.from(form?.monitoringLocationIds ?? []) as location}
-                    <option value={location}>{location}</option>
-                {/each}
-            </select>
-        </div>
-        <div>
-            <button>Calculate</button>
-        </div>
-    </form>
-
-{/if}
-{#if form?.averageWaterTemp}
-    <p>Average Water Temperature for location {form.monitoringLocationId}: {form.averageWaterTemp}°C</p>
-{/if}
+		{#if form?.success}
+			<p>Successfully uploaded file: {form.fileName}</p>
+			<form method="post" action="?/calculate">
+				<div>
+					<label for="location">Select Monitoring Location:</label>
+					<select class="rounded border border-stone-300 px-2 py-1" id="location" name="location">
+						<option class="text-stone-500" value="">--Please Select--</option>
+						{#each Array.from(form?.monitoringLocationIds ?? []) as location}
+							<option value={location}>{location}</option>
+						{/each}
+					</select>
+				</div>
+				<div>
+					<button
+						class="rounded border border-[#d0375b] bg-[#d0375b] px-4 py-2 text-white hover:bg-white hover:text-[#d0375b]"
+						>Calculate</button
+					>
+				</div>
+			</form>
+		{/if}
+		{#if form?.averageWaterTemp}
+			<p>
+				Average Water Temperature for location {form.monitoringLocationId}: {form.averageWaterTemp}°C
+			</p>
+		{/if}
+	</div>
+</div>
